6f36b5c42458d9a942261107de9a1b942330b2ab
Flavia Regina Schuenquer Santos <flavia.schuenquer@t2mlab.com>
2022-07-11 21:34:55 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 
 function App() {
@@ -6,8 +7,15 @@ function App() {
 
     <>
 
-      <h1>Olá Mundo</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+
+          </div>
+        </section>
+      </main>
 
     </>
   )
