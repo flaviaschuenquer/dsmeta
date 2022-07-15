0b44b1c2dc89285c2a9b067e4b02ab9f0da30ea6
Flavia Regina Schuenquer Santos <flavia.schuenquer@t2mlab.com>
2022-07-15 12:26:01 -0300

    Alteração espaço nome

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">Flávia Schuenque/DevSuperior</a>
+                    <a href="https://www.instagram.com/devsuperior.ig"> Flávia Schuenque/DevSuperior</a>
                 </p>
             </div>
         </header>
